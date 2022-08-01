9c5289c57a22e4490d403e98d530a1355dae36a9
Another <835166018@qq.com>
2022-08-01 13:19:42 +0800

    Update workwork.rb

--- workwork.rb
+++ workwork.rb
@@ -4,8 +4,8 @@
 class Workwork < Formula
   desc "开发/测试常用工具CLI"
   homepage "https://github.com/shenguanjiejie/workwork"
-  url "https://github.com/shenguanjiejie/workwork/releases/download/v0.0.1/workwork_0.0.1_darwin.tar.gz"
-  version "0.0.1"
+  url "https://github.com/shenguanjiejie/workwork/releases/download/v0.0.2/workwork_0.0.2_darwin.tar.gz"
+  version "0.0.2"
   sha256 "1fddd3fb797980ecd6e79acc430d2285c8c1a379efe6fc8f4983b68a7987cb2e"
   license "Apache-2.0"
 
